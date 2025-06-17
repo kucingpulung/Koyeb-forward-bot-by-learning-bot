62ef60e47c4f2e89b5f298d86192c78ae6627a3a
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2025-06-17 16:37:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,9 @@
-echo "Cloning Repo...."
-if [ -z $BRANCH ]
-then
-  echo "Cloning main branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
-else
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
-fi
-cd JishuDeveloper/Ultra-Forward-Bot
+#!/bin/bash
+
+cd /Ultra-Forward-Bot || exit 1
+
+# Install dependencies (jaga-jaga)
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
+
+echo "Starting Bot dan Webserver (Uptime Robot)..."
+python main.py
